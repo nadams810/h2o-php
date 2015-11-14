dc4c3de5d40b93b5652dfb46512db0e2cbece9d4
Nathan Adams <adamsna@datanethost.net>
2015-11-14 16:46:53 -0600

    Needed to require parser when loading files from cache (could be a by product of using autoloading on PHP 5.5)

--- h2o.php
+++ h2o.php
@@ -10,6 +10,7 @@ require H2O_ROOT.'h2o/tags.php';
 require H2O_ROOT.'h2o/errors.php';
 require H2O_ROOT.'h2o/filters.php';
 require H2O_ROOT.'h2o/context.php';
+require H2O_ROOT.'h2o/parser.php';
 
 /**
  * Example:
